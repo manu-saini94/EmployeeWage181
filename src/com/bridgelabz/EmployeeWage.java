c504f2c88b3ad70302a1cfec14d85c794c716b22
manu-saini94 <manu.saini94@gmail.com>
2022-08-03 09:48:14 +0530

    Added : employee attendence

--- src/com/bridgelabz/EmployeeWage.java
+++ src/com/bridgelabz/EmployeeWage.java
@@ -3,5 +3,11 @@ package com.bridgelabz;
 public class EmployeeWage {
     public static void main(String[] args) {
         System.out.println("Welcome to EmployeeWage");
+        int empPresent = (int) Math.floor(Math.random() * 10) % 2;    // 0.01  , 0.356  , 0.563
+        if (empPresent == 1) {
+            System.out.println("Employee is Present");
+        } else {
+            System.out.println("Employee is Absent");
+        }
     }
 }
